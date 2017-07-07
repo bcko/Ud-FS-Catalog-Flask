d4d221345f82dccfd898c7d12f2b96fd6db055e0
bc ko <gobcko@gmail.com>
2017-07-07 12:29:32 -0400

    package

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-!#/usr/bin/python3
+from setuptools import setup
+
+setup(
+    name='catalog_app',
+    packages=['catalog_app'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
